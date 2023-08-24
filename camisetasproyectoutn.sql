de0da4b9de2968d6c8ebdad17ee6f20bb9f7d184
nahuel echeto <echetonahuel@gmail.com>
2023-08-23 23:47:52 -0300

    proyecto final

--- camisetasproyectoutn.sql
+++ camisetasproyectoutn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 28-07-2023 a las 17:54:30
+-- Tiempo de generación: 24-08-2023 a las 02:36:43
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.26
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `novedades`
+--
+
+DROP TABLE IF EXISTS `novedades`;
+CREATE TABLE IF NOT EXISTS `novedades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(250) NOT NULL,
+  `subtitulo` varchar(250) NOT NULL,
+  `cuerpo` text NOT NULL,
+  `img_id` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Volcado de datos para la tabla `novedades`
+--
+
+INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
+(6, 'Nueva camiseta de Boca', 'Conseguí la nueva camiseta de Boca Juniors', 'La nueva camiseta de Boca Juniors ya esta disponible en nuestra tienda.', 'qmmucvnkucai3uf9z0bp'),
+(5, 'River campeón !! ', 'Conseguí acá las camisetas de actual campeón del torneo de primera división', '', 'b3gapqorjuwn3aqo2tjc');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -33,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `usuario` varchar(250) NOT NULL,
   `password` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -41,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
 (1, 'nahuel', '81dc9bdb52d04dc20036dbd8313ed055'),
-(2, 'agustin', '81dc9bdb52d04dc20036dbd8313ed055');
+(2, 'flavia', '81dc9bdb52d04dc20036dbd8313ed055');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
